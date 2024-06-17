5cb8f55c5fb56c5b6148d81240a34545f8d3b078
kamilkonefke <kamilkonefke@outlook.com>
2024-06-17 17:41:42 +0200

    Fr

--- nvim/after/plugin/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<Space>f', builtin.find_files, {})
-vim.keymap.set('n', '<Space>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
