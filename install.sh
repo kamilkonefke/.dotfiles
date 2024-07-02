cd4f624060a8febf059c657564f960584effaad5
Kamil Konefke <kamilkonefke@outlook.com>
2024-07-03 01:00:56 +0200

    added some stuff

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 packages=(
-    "sway" "git" "starship" "zsh" "kitty" "fastfetch" "wl-clipboard" "wayland" "ly" "neovim"
-    "pipewire" "pipewire-pulse" "pipewire-jack" "pavucontrol" "firefox-developer-edition"
+    "sway" "swaybg" "xorg-xwayland" "git" "starship" "zsh" "kitty" "fastfetch" "wl-clipboard" "wayland" "ly" "neovim" "ttf-firacode-nerd" "autotiling"
+    "pipewire" "pipewire-pulse" "pipewire-jack" "pavucontrol" "firefox-developer-edition" "slurp" "grim"
 )
 
 # Install all packages
@@ -11,5 +11,8 @@ yay -S ${packages[@]}
 # Copy configs
 cp -rf ./.config ~/ &
 cp -rf ./.zshrc ~/ &
+
+# Enable ly
+systemctl enable ly
 
 echo :3
