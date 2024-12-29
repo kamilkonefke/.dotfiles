41fdbb6c4e7f5994422d108f19e5377fba8ebace
Kamil Konefke <kamilkonefke@outlook.com>
2024-12-29 01:23:41 +0100

    Ghostty

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,17 +1,17 @@
 local lsp = require('lsp-zero')
 -- Setup language servers
 require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.clangd.setup{}
+require'lspconfig'.ccls.setup{}
 require'lspconfig'.gopls.setup{}
 
 -- Keybinds
 local cmp = require('cmp')
 cmp.setup({
     mapping = {
-        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
 })
 
